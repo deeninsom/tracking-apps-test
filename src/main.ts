5fbbc36620e4f435894eb7aa6fbf33d5b0b6172a
deeninsom <syihab855@gmail.com>
2024-01-30 03:35:00 +0700

    test conenction socket

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 // import * as cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
     credentials: true
--- src/modules/socket/socket.service.ts
+++ src/modules/socket/socket.service.ts
@@ -1,64 +1,69 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import * as chalk from 'chalk';
 
-
-@WebSocketGateway({ cors: {
-  origin: "*",
-  methods: ["GET", "POST"],
-  transports: ['websocket', 'polling'],
-  credentials: true
-}, namespace: "/api/socket" })
-export class SocketGateway {
+@WebSocketGateway({
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    transports: ['websocket', 'polling'],
+    credentials: true
+  },
+  namespace: "/api/socket",
+  transports: ['websocket']
+})
+export class SocketGateway implements OnGatewayConnection {
   @WebSocketServer() server: Server;
   private userLocations: Record<string, any> = {};
   private logger: Logger = new Logger('SocketGateway');
 
-  handleConnection(client: Socket) {
+  handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(chalk.yellowBright(`Socket is connected: ${client.id}`));
 
-    // client.on('send-locations', (data: any) => {
-    //   this.server.emit('received-locations', {data});
-    // });
-
+    // Additional code for handling connections, if needed
   }
-
-  // @SubscribeMessage('message')
-  // socketGetUsers(client: any, payload: any): string {
-  //   console.log(payload)
-  //   return 'Hello world!';
-  // }
-
-  // handleConnection(client: Socket): void {
-  //   console.log('A user connected');
-
-  //   if (client.handshake.auth.userId) {
-  //     const initialLocation = client.handshake.auth.location;
-  //     if (initialLocation) {
-  //       this.userLocations[client.id] = initialLocation;
-  //       this.server.emit('locationUpdate', { id: client.id, location: initialLocation });
-  //     }
-  //   }
-
-  //   client.on('locationUpdate', (location: any) => {
-  //     console.log('Received location update:', location);
-
-  //     this.userLocations[client.id] = location;
-
-  //     client.broadcast.emit('locationUpdate', { id: client.id, location });
-  //   });
-
-  // }
-
-  // handleDisconnect(client: Socket): void {
-  //   console.log('User disconnected');
-
-  //   delete this.userLocations[client.id];
-
-  //   this.server.emit('userDisconnected', client.id);
-  // }
 }
+
+// client.on('send-locations', (data: any) => {
+//   this.server.emit('received-locations', {data});
+// });
+
+
+// @SubscribeMessage('message')
+// socketGetUsers(client: any, payload: any): string {
+//   console.log(payload)
+//   return 'Hello world!';
+// }
+
+// handleConnection(client: Socket): void {
+//   console.log('A user connected');
+
+//   if (client.handshake.auth.userId) {
+//     const initialLocation = client.handshake.auth.location;
+//     if (initialLocation) {
+//       this.userLocations[client.id] = initialLocation;
+//       this.server.emit('locationUpdate', { id: client.id, location: initialLocation });
+//     }
+//   }
+
+//   client.on('locationUpdate', (location: any) => {
+//     console.log('Received location update:', location);
+
+//     this.userLocations[client.id] = location;
+
+//     client.broadcast.emit('locationUpdate', { id: client.id, location });
+//   });
+
+// }
+
+// handleDisconnect(client: Socket): void {
+//   console.log('User disconnected');
+
+//   delete this.userLocations[client.id];
+
+//   this.server.emit('userDisconnected', client.id);
+// }
 
 
 // import { SubscribeMessage, WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
