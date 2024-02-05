c3688e43b34f34dce900e631bbb0c248be3f2bd9
deeninsom <syihab855@gmail.com>
2024-02-06 01:10:14 +0700

    fix: add relation  user

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -7,6 +7,7 @@ import {
   OneToMany,
 } from 'typeorm';
 import UserLocations from '../location-user/location-user.entity';
+import Tasks from '../task/task.entity';
 
 
 export enum Role {
@@ -56,6 +57,14 @@ export default class Users {
     onUpdate: 'CASCADE',
   })
   location_users: UserLocations[];
+
+  @OneToMany(() => Tasks, (task) => task.user_id, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  tasks: Tasks[];
+
 
   @CreateDateColumn()
   public created_at: Date;
