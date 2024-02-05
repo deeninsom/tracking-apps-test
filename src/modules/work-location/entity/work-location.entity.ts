fb7e948816289322f253d3744b8ef0c07001416b
deeninsom <syihab855@gmail.com>
2024-02-06 01:11:11 +0700

    fix: add relation to task

--- src/modules/work-location/entity/work-location.entity.ts
+++ src/modules/work-location/entity/work-location.entity.ts
@@ -7,6 +7,7 @@ import {
   OneToMany,
 } from 'typeorm';
 import WorkLocationLists from './work.location-list.entity';
+import Tasks from 'src/modules/task/task.entity';
 
 @Entity()
 export default class WorkLocations {
@@ -25,6 +26,14 @@ export default class WorkLocations {
     onUpdate: 'CASCADE',
   })
   location_list: WorkLocationLists[];
+
+  @OneToMany(() => Tasks, (task) => task.location_id, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  tasks: Tasks[];
+
 
   @CreateDateColumn()
   public created_at: Date;
