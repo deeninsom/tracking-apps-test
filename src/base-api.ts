faa4316810fa1dd2440174b174f9833b911d9303
deeninsom <syihab855@gmail.com>
2024-02-10 17:02:49 +0700

    test ip

--- src/base-api.ts
+++ src/base-api.ts
@@ -7,7 +7,19 @@ export class AppController {
 
   @Get()
   getHello(@Req() req: Request, @Res() res: Response) {
-    const api = req.ip
-    res.send(api)
+    // Mengambil alamat IP dari header x-forwarded-for
+    const forwardedFor: any = req.headers['x-forwarded-for'];
+    let userIp = '';
+    if (forwardedFor) {
+      // Header x-forwarded-for bisa berisi beberapa alamat IP yang dipisahkan oleh koma
+      // Alamat IP pertama dalam daftar adalah alamat IP asli pengguna
+      userIp = forwardedFor.split(',')[0];
+    } else {
+      // Jika header x-forwarded-for tidak ada, gunakan alamat IP langsung dari request
+      userIp = req.ip;
+    }
+    
+    // Mengirimkan alamat IP pengguna sebagai respons
+    res.send(userIp);
   }
 }
