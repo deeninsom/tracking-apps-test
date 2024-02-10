73b147b585f94bfb936caaaf532d34189ad1848a
deeninsom <syihab855@gmail.com>
2024-02-10 17:10:43 +0700

    get ip

--- src/base-api.ts
+++ src/base-api.ts
@@ -1,25 +1,13 @@
 import { Controller, Get, Req, Res } from "@nestjs/common";
 import { Request, Response } from "express";
-
+import { getClientIp } from "request-ip";
 @Controller('/')
 export class AppController {
   constructor() {}
 
   @Get()
   getHello(@Req() req: Request, @Res() res: Response) {
-    // Mengambil alamat IP dari header x-forwarded-for
-    const forwardedFor: any = req.headers['x-forwarded-for'];
-    let userIp = '';
-    if (forwardedFor) {
-      // Header x-forwarded-for bisa berisi beberapa alamat IP yang dipisahkan oleh koma
-      // Alamat IP pertama dalam daftar adalah alamat IP asli pengguna
-      userIp = forwardedFor.split(',')[0];
-    } else {
-      // Jika header x-forwarded-for tidak ada, gunakan alamat IP langsung dari request
-      userIp = req.ip;
-    }
-    
-    // Mengirimkan alamat IP pengguna sebagai respons
-    res.send(userIp);
+    const clientIp: any = getClientIp(req)
+    res.send(`Your IP Address is ${clientIp}.`)
   }
 }
