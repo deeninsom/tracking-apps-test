4fec0ebdbd5d108f1b17436958e2b6b8e42eff80
deeninsom <syihab855@gmail.com>
2024-02-24 01:30:37 +0700

    fix: handle error if lat lng not found from google api

--- src/utils/getAddressComponents.ts
+++ src/utils/getAddressComponents.ts
@@ -1,9 +1,18 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import axios from 'axios';
 
 export const getAddressComponents = async (lat: any, lng: any) => {
-  const googleMapsResponse = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_KEY}`,
-  );
-  const locationJson = JSON.stringify(googleMapsResponse.data.results[3]);
-  return locationJson;
+  try {
+    const googleMapsResponse = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_KEY}`,
+    );
+
+    const locationJson = JSON.stringify(googleMapsResponse.data.results[3]);
+    return locationJson;
+  } catch (error: any) {
+    throw new HttpException(
+      `Lokasi dengan lattitude : ${lat} dan longtittude : ${lng} tidak ditemukan !`,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
 };
